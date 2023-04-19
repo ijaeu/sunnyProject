abb29ce2ecbc1919480398c22d7cfc49f29271fc
Jmsuhhh <min567567@naver.com>
2023-04-19 13:32:33 +0900

    비밀번호 값이 올바르지 않을 때 alert창 띄우고 폼 제출 막기/모든 검사 통과시 폼 제출 기능 완료

--- WebContent/assets/js/rePwOk.js
+++ WebContent/assets/js/rePwOk.js
@@ -2,10 +2,10 @@
  * 
  */
 
- /*비밀번호 일치 여부 검사*/
+/*비밀번호 일치 여부 검사*/
 // inputPw 값이 변경될 때마다 실행
 $('.inputPw').on('keyup', function () {
-  var password = $('input[name=newPassword]').val(); // 비밀번호 값
+  var password = $('#newPassword').val(); // 비밀번호 값
   var passwordConfirm = $(this).val(); // 비밀번호 확인 값
   var errorSpan = $(this).siblings('.password-error'); // 오류 메시지 span
 
@@ -18,14 +18,14 @@ $('.inputPw').on('keyup', function () {
 
 
 /*비밀번호 정규표현식*/
- let $checkPwMsg = $("#check-pw-msg");
- let $pwInput = $("#newPassword");
- 
- // 대소문자 상관 없이 영어가 포함, 숫자 포함, 특수문자 포함, 8글자 이상
+let $checkPwMsg = $("#check-pw-msg");
+let $pwInput = $("#newPassword");
+
+// 대소문자 상관 없이 영어가 포함, 숫자 포함, 특수문자 포함, 8글자 이상
 const regex4 = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[!@#$%^&*()_+])[a-zA-Z\d!@#$%^&*()_+]{8,}$/;
 
 $pwInput.on('blur', function(){
-   if(regex4.test(  $(this).val()   )){
+   if(regex4.test($(this).val())){
       $checkPwMsg.text("사용 가능한 비밀번호입니다.");
    }else{
       $checkPwMsg.html("사용 불가능한 비밀번호입니다. <br>영어, 숫자, 특수문자를 포함하여 8글자 이상 작성하세요!");
@@ -34,15 +34,15 @@ $pwInput.on('blur', function(){
 
 
 
-/*비밀번호 일치 확인검사와 정규표현식을 통과하지 못했을 때 폼 제출 막기*/
+/*비밀번호 일치 확인 검사와 정규표현식을 통과하지 못했을 때 폼 제출 막기*/
 
 $(document).ready(function() {
   $('form').submit(function(event) {
     event.preventDefault(); // 기본 동작인 폼 제출을 막음
 
-    var passwordInput = $('input[name=password]');
+    var passwordInput = $('#newPassword');
     var confirmPasswordInput = $('input[name=confirmPassword]');
-    var errorSpan = $('input[name=confirmPassword]').siblings('.password-error');
+    var errorSpan = confirmPasswordInput.siblings('.password-error');
 
     var password = passwordInput.val();
     var confirmPassword = confirmPasswordInput.val();
@@ -55,7 +55,7 @@ $(document).ready(function() {
     }
 
     if (password !== confirmPassword) {
-      alert('비밀번호 입력값이 올바르지 않습니다.'); // 비밀번호 확인이 일치하지 않을 때 경고창 출력
+      alert('비밀번호 입력값이 일치하지 않습니다.'); // 비밀번호 확인이 일치하지 않을 때 경고창 출력
       return;
     }
 
